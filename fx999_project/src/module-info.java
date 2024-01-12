614c3b00ede530bbd86092dfe80840cee3434d13
qwerdf1133 <anwltjdwhdgus@gmail.com>
2024-01-12 16:23:40 +0900

    2조 프로젝트 수정 + 필요 없는 폴더 제거

--- fx999_project/src/module-info.java
+++
@@ -1,9 +0,0 @@
-open module fx999_project {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.web;
-	requires java.sql;
-	requires java.desktop;
-	
-//	opens pay to javafx.graphics, javafx.fxml;
-}
